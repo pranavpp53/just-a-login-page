b67b1211ab4bb75ab83c9cfff03dc42baac78610
pranav <pppranav53@gmail.com>
2026-06-04 15:07:29 +0530

    bug in deploy fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const dataFile = path.join(__dirname, "logins.json");
 const signupFile = path.join(__dirname, "signups.json");
 
